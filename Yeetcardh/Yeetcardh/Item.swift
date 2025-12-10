05f95401140ac845e3d8a62fca744b8d86aa331d
Rishi Malik <rishi@rishim.com>
2025-12-09 18:59:30 -0700

    Initial commit, empty xcode project

---
+++ Yeetcardh/Yeetcardh/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Yeetcardh
+//
+//  Created by Rishi Malik on 12/9/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Yeetcardh/YeetcardhTests/YeetcardhTests.swift
@@ -0,0 +1,16 @@
+//
+//  YeetcardhTests.swift
+//  YeetcardhTests
+//
+//  Created by Rishi Malik on 12/9/25.
+//
+
+import Testing
+
+struct YeetcardhTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
